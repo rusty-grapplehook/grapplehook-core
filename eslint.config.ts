b68f029586e11ba3132c2f8a3d0aa603ccd86784
kitsupanic <kitsupanic@gmail.com>
2026-07-07 23:14:49 +0300

    let it ride

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
+  { ignores: ['dist/'] },
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
   { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
